8cb51b1b0358059c2aae1c4eb7c74b00b8b86f36
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-10-21 22:40:28 +0000

    fix: improvements

--- main.go
+++ main.go
@@ -11,8 +11,13 @@ import (
 	"github.com/brutella/hap"
 	"github.com/brutella/hap/accessory"
 	"github.com/brutella/hap/characteristic"
+	"github.com/caarlos0/env/v9"
 	"github.com/charmbracelet/log"
 )
+
+type Config struct {
+	Addr string `env:"LISTEN" envDefault:":8089"`
+}
 
 func main() {
 	bridge := accessory.NewBridge(accessory.Info{
@@ -45,24 +50,33 @@ func main() {
 	for i, a := range []*LeakSensor{leak1, leak2} {
 		server.ServeMux().
 			HandleFunc(fmt.Sprintf("/leak/%d/detected", i+1), func(_ http.ResponseWriter, _ *http.Request) {
+				log.Info("status", "sensor", a.Name(), "leaking", true)
 				a.LeakSensor.LeakDetected.SetValue(characteristic.LeakDetectedLeakDetected)
 			})
 		server.ServeMux().
 			HandleFunc(fmt.Sprintf("/leak/%d/cleared", i+1), func(_ http.ResponseWriter, _ *http.Request) {
+				log.Info("status", "sensor", a.Name(), "leaking", false)
 				a.LeakSensor.LeakDetected.SetValue(characteristic.LeakDetectedLeakNotDetected)
 			})
 	}
 
 	server.ServeMux().
 		HandleFunc(fmt.Sprintf("/smoke/%d/detected", 1), func(_ http.ResponseWriter, _ *http.Request) {
+			log.Info("status", "sensor", smoke1.Name(), "smoke", true)
 			smoke1.SmokeSensor.SmokeDetected.SetValue(characteristic.SmokeDetectedSmokeDetected)
 		})
 	server.ServeMux().
 		HandleFunc(fmt.Sprintf("/smoke/%d/cleared", 1), func(_ http.ResponseWriter, _ *http.Request) {
+			log.Info("status", "sensor", smoke1.Name(), "smoke", false)
 			smoke1.SmokeSensor.SmokeDetected.SetValue(characteristic.SmokeDetectedSmokeNotDetected)
 		})
 
-	server.Addr = ":9098"
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal("cannot parse config", "err", err)
+	}
+
+	server.Addr = cfg.Addr
 
 	log.Info("server started", "addr", server.Addr)
 
